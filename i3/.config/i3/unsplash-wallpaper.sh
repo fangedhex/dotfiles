fdd3296dd9b274afac2fcfae58750184013da8dd
Sylvain VISTE <viste.sylvain@gmail.com>
2018-04-09 14:34:27 +0200

    Readding -L to curl command to handle redirect requests

--- i3/.config/i3/unsplash-wallpaper.sh
+++ i3/.config/i3/unsplash-wallpaper.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl "https://source.unsplash.com/featured/1920x1080/daily/?landscape" | feh --bg-scale -
+curl -L "https://source.unsplash.com/featured/1920x1080/daily/?landscape" | feh --bg-scale -
