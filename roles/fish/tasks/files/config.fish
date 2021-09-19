0d21245584de75a6440c8001ac581b4015b0388f
Sylvain VISTE <viste.sylvain@gmail.com>
2021-09-19 13:20:06 +0200

    reimplementing old dotfiles with ansible

---
+++ roles/fish/tasks/files/config.fish
@@ -0,0 +1,2 @@
+set -x EDITOR /usr/bin/vim
+set -x fish_color_autosuggestion 7A7A7A
---
+++ roles/i3wm/files/unsplash-wallpaper.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -L "https://source.unsplash.com/featured/1920x1080/daily/?landscape" | feh --bg-scale -
